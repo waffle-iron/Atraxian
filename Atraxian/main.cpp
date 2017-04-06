35bdb6ad13245d8440e8ccb070c8404eb671d45d
kennyrkun <keennymccormic+github@gmail.com>
2017-04-05 22:39:20 -0500

    upadated main.cpp
    creates an environment, and deletes it when finished.

--- Atraxian/main.cpp
+++ Atraxian/main.cpp
@@ -1,4 +1,19 @@
+#include <SFML\Graphics.hpp>
+
+#include "Environment.hpp"
+#include "logger.hpp"
+
+#include <iostream>
+
 int main()
 {
+	logger::INFO("Initlializing Atraxian " ENVIRONMENT_VERSION "...");
+
+	Environment *atraxia = new Environment(sf::VideoMode(1080, 720), "AtraxiaN-" ENVIRONMENT_VERSION);
+	atraxia->main();
+
+	delete atraxia;
+
+	logger::INFO("exiting...");
 	return 0;
 }

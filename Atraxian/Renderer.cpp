d3ecb94845048905e5b4ba9ef0ff40d5094e65f8
kennyrkun <keennymccormic+github@gmail.com>
2017-04-07 19:42:50 -0500

    implemented renderer
    - only renders rectangles until I can figure out how to use sf::Drawable as an argument and a vector.

--- Atraxian/Renderer.cpp
+++ Atraxian/Renderer.cpp
@@ -1,11 +1,30 @@
 #include "Renderer.hpp"
+#include "logger.hpp"
 
-Renderer::Renderer()
+Renderer::Renderer(sf::RenderWindow *target_window)
 {
+	logger::INFO("New Renderer instance created.");
 
+	window = target_window;
 }
 
 Renderer::~Renderer()
 {
+	logger::INFO("Renderer isntance destroyed.");
+}
 
+void Renderer::addToQueue(sf::RectangleShape &object)
+{
+ 	render_queue.push_back(object);
+}
+
+void Renderer::render()
+{
+	if (!render_queue.empty())
+	{
+		for (size_t i = 0; i < render_queue.size(); i++)
+		{
+			window->draw(render_queue[i]);
+		}
+	}
 }
--- Atraxian/Renderer.hpp
+++ Atraxian/Renderer.hpp
@@ -1,11 +1,22 @@
 #ifndef RENDERER_HPP
 #define RENDERER_HPP
 
+#include <SFML\Graphics.hpp>
+
+#include <vector>
+
 class Renderer
 {
 public:
-	Renderer();
+	Renderer(sf::RenderWindow *target_window);
 	~Renderer();
+
+	void addToQueue(sf::RectangleShape &object); //  |   > OVERLOAD
+	void render();
+
+private:
+	sf::RenderWindow *window;
+	std::vector<sf::RectangleShape> render_queue;
 };
 
 #endif
